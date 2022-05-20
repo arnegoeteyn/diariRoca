1f753b4ed96579f1f6fa290db8ae81959ff2d2e8
Arne Goeteyn <arne@moonchase.be>
2022-05-20 16:06:55 +0200

    add srvice worker

--- src/index.js
+++ src/index.js
@@ -18,12 +18,9 @@ app.ports.storeCache.subscribe(function (val) {
   } else {
     localStorage.setItem(storageKey, JSON.stringify(val));
   }
-
-  // Report that the new session was stored successfully.
-  // setTimeout(function () { app.ports.onStoreChange.send(val); }, 0);
 });
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
